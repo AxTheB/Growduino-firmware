f22337e511b8e0d68f03c9f50057c21c6b642b51
Václav 'Ax' Hůla <axtheb@gmail.com>
2016-08-25 20:47:36 +0200

    More robust UPS serial reading

--- ups.cpp
+++ ups.cpp
@@ -12,32 +12,55 @@ int ups_read_inner(){
     int energy;
     int counter;
     // = Serial3.readBytesUntil('s', line, 64);
-    counter = Serial3.readBytesUntil('\n', line, 64);  //throw away first line, as it may not be complete
-    strcpy(line, "--------------------------------");
+    //counter = Serial3.readBytesUntil('\n', line, 64);  //throw away first line, as it may not be complete
 
-    counter = Serial3.readBytesUntil('\n', line, 64);
+    if (Serial3.available()) {
+        strcpy(line, "................................................................");
 
-    if (line[0] != 's') { // UPS stats start with 's'
-        // UPS error handling may come here
-        return MINVALUE;
-    }
-
-    line[counter]='\0';
+        counter = Serial3.readBytesUntil('\n', line, 64);
 #ifdef DEBUG_UPS
-    SERIAL.println(line);
+        SERIAL.print("Reading UPS: ");
+        SERIAL.println(line);
 #endif
-    sscanf(line, "s:%i e:%i", &state, &energy);
-    if (energy < 0 || energy > 100) {
+
+        if (line[0] != 's') { // UPS stats start with 's'
+            // UPS error handling may come here
+#ifdef DEBUG_UPS
+            SERIAL.println("Wrong line start");
+#endif
+            return MINVALUE;
+        }
+
+        line[counter]='\0';
+        sscanf(line, "s:%i e:%i", &state, &energy);
+        if (energy < 0 || energy > 100) {
+#ifdef DEBUG_UPS
+            SERIAL.println("Value out of bounds");
+#endif
+            energy = MINVALUE;
+        }
+#ifdef DEBUG_UPS
+        SERIAL.println("Value OK");
+#endif
+        ups_level = state;
+    } else {
+#ifdef DEBUG_UPS
+        SERIAL.println("Serial buffer empty");
         energy = MINVALUE;
     }
-    ups_level = state;
+#endif
+
 
     return energy;
 }
 
 int ups_read(){
     int retval;
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 10; i++) {
+#ifdef DEBUG_UPS
+        SERIAL.print("UPS reading try #");
+        SERIAL.println(i+1);
+#endif
         retval = ups_read_inner();
         if (retval != MINVALUE) {
             return retval;
